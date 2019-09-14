2013f13c279cae8d89e118a348517bff22987b5f
harshalakmalmadhu <harshalakmal.hm@gmail.com>
2019-09-14 12:02:16 +0530

    add user registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { UserService } from './shared/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/user.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private fb:FormBuilder,private http:HttpClient) { }
+
+  //readonly BaseURI = 'http://localhost:3032/insertDocPar?id=500&document_id=abdabsdd&patient_id=20493&tenant=QATAR';
+  readonly BaseURI = 'http://localhost:3032';
+
+
+
+
+  formModel = this.fb.group({
+    UserName: ['',Validators.required],
+    Email: ['',[Validators.required,Validators.email]],
+    FullName: [''],
+    Passwords: this.fb.group({
+      Password: ['',[Validators.required,Validators.minLength(4)]],
+      ConfirmPassword: ['',Validators.required]
+
+    },{validator : this.comparePasswords})
+
+
+
+  });
+  comparePasswords(fb:FormGroup){
+    let ConfirmPasswordCtrl = fb.get('ConfirmPassword');
+    if(ConfirmPasswordCtrl.errors == null || 'passwordMissmatch' in ConfirmPasswordCtrl.errors ){
+      if(fb.get('Password').value!=ConfirmPasswordCtrl.value)
+      ConfirmPasswordCtrl.setErrors({passwordMissmatch:true});
+      else
+      ConfirmPasswordCtrl.setErrors(null);
+    }
+  }
+  register(){
+    var body = {
+      UserName:this.formModel.value.UserName,
+      Email:this.formModel.value.Email,
+      FullName:this.formModel.value.FullName,
+      Password:this.formModel.value.Passwords.Password,
+    };
+    return this.http.post(this.BaseURI+'/insertBody',body);
+
+  }
+
+}
